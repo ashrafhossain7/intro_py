1569db982443487c0f319c4926c76ec3023976b8
ashraf <ashrafhossian97531@gmail.com>
2022-06-27 14:12:45 +0600

    v1

---
+++ helloworld/1sttask.py
@@ -0,0 +1,9 @@
+price =10
+rating =4.9
+name= 'rifat'
+is_published = True
+print (price, rating , name, is_published)
+name='john smith'
+age=20
+patient=True
+print (name , age , "patient is new ",patient)
---
+++ helloworld/app.py
@@ -0,0 +1,4 @@
+print("ashraf hossain")
+print('o----')
+print ('||||||')
+print("*" * 10)# it is like multiplication
